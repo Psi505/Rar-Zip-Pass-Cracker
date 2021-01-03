4507a0d429404c15f3732c41fdbfcdd725bdb483
Psi505 <hamzusguesmi@gamil.com>
2021-01-03 03:13:22 +0100

    update

--- Rar Pass Cracker.bat
+++ Rar Pass Cracker.bat
@@ -15,11 +15,11 @@ echo      /_/    \_,_/ /___//___/    \___/  /_/   \_,_/ \__/ /_/\_\ \__/ /_/
 echo.
 echo.
 echo.
-echo        Author  : Psi505    ^| Description : This is a simple batch program
-echo        Version : 1.1       ^| to help you cracking passwords of Rar/Zip/7z
+echo        Author  : Psi505    ^|  Description : This is a simple batch program
+echo        Version : 1.2       ^|  to help you crack Rar/Zip/7z passwords.
 echo.
 echo   :=-----------------------------------------------------------------------=:
-pause>nul
+timeout /t 8 >nul
 ::-----------------------------------::
 
 ::-------------- prog ---------------::
@@ -77,47 +77,66 @@ echo %arch_name% | findstr /c:".rar" 1>nul && set "fol_name=%arch_name:.rar=%"
 echo %arch_name% | findstr /c:".zip" 1>nul && set "fol_name=%arch_name:.zip=%"
 echo %arch_name% | findstr /c:".7z" 1>nul && set "fol_name=%arch_name:.7z=%"
 
-md "%fol_name%"
-set /p p=Press enter key to start cracking...
+if "%fol_name%"=="wordlists" (set fol_name=%fol_name%0)
+rd /q /s "%fol_name%" >nul 2>&1
+md "%fol_name%" 2>nul 
 
 :: Crack the password
-for /f usebackq %%i in ("%wl_name%") do (set /a nb0+=1)
-echo %arch_name% | findstr /c:".rar" 1>nul && (
-    :: crack rar 
-    if not exist unrar.exe (certutil -decode unrar unrar.exe 1>nul)
-    for /f "tokens=* usebackq" %%i in ("%wl_name%") do (
-        cls
-        UnRAR.exe x -inul -P"%%i" "%arch_name%" "%fol_name%" && (set passwd=%%i && goto exitfor)
-        set /a nb1+=1
-        @title Tested passwords : !nb1! / !nb0! 
+cls && echo Counting provided passwords...
+for /f %%i in ('powershell -command ^(Get-Content %wl_name%^).Length') do (set /a nb0=%%i)
+cls && echo  -^> !nb0! password were found ! && echo.
+set /p p=Press enter key to start cracking...
+
+cls && echo %arch_name% | findstr /c:".rar" 1>nul && (
+    :: crack rar
+    data\unrar.exe x -inul -P"" "%arch_name%" "%fol_name%" || (
+        for /f "tokens=* usebackq" %%i in ("%wl_name%") do (
+            data\unrar.exe x -inul -P"%%i" "%arch_name%" "%fol_name%" && (set passwd=%%i && goto exitfor)
+            set /a nb1+=1
+            @title Tested passwords : !nb1! / !nb0! 
+        )
     )
+    goto not-encrypted
 ) || (
     :: crack zip/7z
-    if not exist 7z.exe (certutil -decode 7z 7z.exe 1>nul)
-    for /f "tokens=* usebackq" %%i in ("%wl_name%") do (
-        cls
-        7z x "%arch_name%" -o"%fol_name%" -P"%%i" -y >nul 2>&1 && (set passwd=%%i && goto exitfor)
-        set /a nb1+=1
-        @title Tested passwords : !nb1! / !nb0! 
+    data\7z.exe x "%arch_name%" -o"%fol_name%" -P"" -y >nul 2>&1 || (
+        for /f "tokens=* usebackq" %%i in ("%wl_name%") do (
+            data\7z.exe x "%arch_name%" -o"%fol_name%" -P"%%i" -y >nul 2>&1 && (set passwd=%%i && goto exitfor)
+            set /a nb1+=1
+            @title Tested passwords : !nb1! / !nb0! 
+        )
     )
+    goto not-encrypted
 )
+
+:not-encrypted
+    cls & color a
+    @Title No password encryption was found !!
+    echo + This archive has no password protection :3
+    echo.
+    echo + All files in your archive are extracted in "%fol_name%" folder :^)
+    echo.
+    echo.
+    pause>nul
+Exit
+
 :exitfor
+    if not defined passwd (
+        rd %fol_name%
+        color c
+        echo Sorry, can't find the password using the provided wordlist :/
+        echo Please try with another wordlist !
+        pathping 127.0.0.1 -n -q 1 -p 5000 >nul
+        exit
+    )
 
-if not defined passwd (
-    rd %fol_name%
-    color c
-    echo Sorry, we can't find the password using the provided wordlist :/
-    echo Please try with another wordlist !
-    pathping 127.0.0.1 -n -q 1 -p 5000 >nul
-    exit
-)
-
-cls & color a
-@Title Password successfully cracked !!
-echo All files in your archive are extracted in "%fol_name%" folder :^)
-echo.
-echo.
-echo     --^> The password: !passwd!
-pause>nul
+    cls & color a
+    echo %fol_name%::!passwd!>> cracked-passwords.txt
+    @Title Password successfully cracked !!
+    echo All files in your archive are extracted in "%fol_name%" folder :^)
+    echo.
+    echo.
+    echo     --^> The password: !passwd!
+    pause>nul
 Exit
 ::-----------------------------------::
